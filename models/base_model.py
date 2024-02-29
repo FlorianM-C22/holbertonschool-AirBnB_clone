f19f95f954a6c0a9ad8ca2461875a1389b452049
VandevilleF <flo.vandeville@gmail.com>
2024-02-29 20:09:09 +0100

    Change order of id and import datetime from datetime

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ BaseModel class module
 """
 
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
@@ -17,9 +17,9 @@ class BaseModel:
         """
         Constructor for the BaseModel class
         """
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """
@@ -33,7 +33,7 @@ class BaseModel:
         Updates the public instance attribute updated_at with the current
         datetime
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
