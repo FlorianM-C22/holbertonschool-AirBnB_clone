9da1bf7118e8b16bfcbcfde6c2bc0e00d496eaab
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-29 15:18:16 +0100

    base_model.py - 1st iteration

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Base class module
+
+
+"""
+
+import uuid
+import datetime
+
+
+class BaseModel:
+    """
+    Base class for other classes to be used in the project
+    """
+    def __init__(self):
+        """
+        Constructor for the BaseModel class
+        """
+        self.id = uuid.uuid4()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """
+        Returns a string representation of the object
+        """
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at with the current
+        datetime
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the
+        instance
+        """
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
