6f1a71825776745f0a377a338c64783926a628d2
VandevilleF <flo.vandeville@gmail.com>
2024-02-29 16:52:00 +0100

    Change order of line id, created_at and updated_at in def __init__

--- models/base_model.py
+++ models/base_model.py
@@ -17,9 +17,9 @@ class BaseModel:
         """
         Constructor for the BaseModel class
         """
-        self.updated_at = datetime.datetime.now()
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """
