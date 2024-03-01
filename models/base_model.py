78e752b94004290ce623d7b636f5de95f69c8188
VandevilleF <flo.vandeville@gmail.com>
2024-03-01 10:03:36 +0100

    Add kwarg condition in def __init__ and test_base_model_dict

--- models/base_model.py
+++ models/base_model.py
@@ -15,13 +15,20 @@ class BaseModel:
     Base class for other classes to be used in the project
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Constructor for the BaseModel class
         """
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        self.id = str(uuid.uuid4())
+        if kwargs is not None and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key in ["crearted_at", "updated_at"]:
+                    value = datetime.fromisoformat(value)
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
