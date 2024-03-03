92d34c26b6a6e6b2ad73a363796afa90e342a112
FlorianM-C22 <7484@holbertonstudents.com>
2024-03-03 16:09:41 +0100

    Updated tests for test_base_model

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,15 +1,38 @@
 #!/usr/bin/python3
+"""Unittest for base_model.py"""
+import unittest
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-                                   my_model_json[key]))
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.my_model = BaseModel()
+
+    def test_init(self):
+        self.assertIsInstance(self.my_model, BaseModel)
+        self.assertTrue(hasattr(self.my_model, 'id'))
+        self.assertTrue(hasattr(self.my_model, 'created_at'))
+        self.assertTrue(hasattr(self.my_model, 'updated_at'))
+
+    def test_str(self):
+        expected_str = "[BaseModel] ({}) {}".format(self.my_model.id,
+                                                    self.my_model.__dict__)
+        self.assertEqual(str(self.my_model), expected_str)
+
+    def test_save(self):
+        old_updated_at = self.my_model.updated_at
+        self.my_model.save()
+        self.assertNotEqual(old_updated_at, self.my_model.updated_at)
+
+    def test_to_dict(self):
+        expected_dict = {
+            'id': self.my_model.id,
+            'created_at': self.my_model.created_at.isoformat(),
+            'updated_at': self.my_model.updated_at.isoformat(),
+            '__class__': 'BaseModel'
+        }
+        self.assertEqual(self.my_model.to_dict(), expected_dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
