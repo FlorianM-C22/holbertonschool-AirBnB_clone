1c329e0146cf9728918d6c61460e7efcf21a6cc7
FlorianM-C22 <7484@holbertonstudents.com>
2024-03-04 14:29:01 +0100

    Added test_ser.py

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,35 @@
+import unittest
+from models.user import User
+from models.base_model import BaseModel
+
+
+class TestUser(unittest.TestCase):
+
+    def test_inheritance(self):
+        user = User()
+        self.assertIsInstance(user, BaseModel)
+
+    def test_attributes(self):
+        user = User()
+        self.assertEqual(user.email, "")
+        self.assertEqual(user.password, "")
+        self.assertEqual(user.first_name, "")
+        self.assertEqual(user.last_name, "")
+
+    def test_attribute_types(self):
+        user = User()
+        self.assertIsInstance(user.email, str)
+        self.assertIsInstance(user.password, str)
+        self.assertIsInstance(user.first_name, str)
+        self.assertIsInstance(user.last_name, str)
+
+    def test_attribute_defaults(self):
+        user = User()
+        self.assertEqual(user.email, "")
+        self.assertEqual(user.password, "")
+        self.assertEqual(user.first_name, "")
+        self.assertEqual(user.last_name, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
