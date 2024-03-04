cc3eb970fef4716e9b837cc72ed0d3fb29e33561
FlorianM-C22 <7484@holbertonstudents.com>
2024-03-04 17:23:16 +0100

    Updated console with proper imports

--- console.py
+++ console.py
@@ -6,6 +6,12 @@ import cmd
 import sys
 import shlex
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
 
 
@@ -16,11 +22,11 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     class_dict = {
         "BaseModel": BaseModel,
-        "User": "User",
-        "State": "State",
-        "City": "City",
-        "Amenity": "Amenity",
-        "Place": "Place",
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
     }
 
     def do_quit(self, arg):
